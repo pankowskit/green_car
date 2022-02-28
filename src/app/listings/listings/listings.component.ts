40fe84cb647c8f541c1bd5debc254395faa5783b
pankowskitomasz <pankowski.email@gmail.com>
2022-02-28 17:02:33 +0200

    routing

---
+++ src/app/listings/listings/listings.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listings',
+  templateUrl: './listings.component.html',
+  styleUrls: [
+    './listings.component.scss',
+    "../../sass/styles.scss"
+  ]
+})
+export class ListingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
